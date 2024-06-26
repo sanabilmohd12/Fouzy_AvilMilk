0692ffef1ccab7190c41d1f58f392a77b6fc75ee
HIBA RASHID <am5291667@gmail.com>
2024-06-26 21:01:46 +0530

    hiba

--- lib/provider/mainprovider.dart
+++ lib/provider/mainprovider.dart
@@ -1,17 +1,13 @@
 
 
-
-
-import 'dart:collection';
-
 import 'package:flutter/cupertino.dart';
 
 class Mainprovider extends ChangeNotifier{
 
-Future<void>addItems()async{
-  String id = DateTime.now().millisecondsSinceEpoch.toString();
-HashMap<String,Object> map = HashMap();
-  map["USER_ID"] = id;
-}
+
+
+  /// add fun for maincategory
+
+
   
 }
