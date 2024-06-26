6a09c4b3f391096ccbd5246bbe1b97c04cd4b69e
Anascholothil <anasbinummer80@gmail.com>
2024-06-26 10:55:53 +0530

    anas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fouzy/view/homescreen.dart';
 
 
 void main() {
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home:  Splashscreen(),
-      home:  BottomNavBar(),
+      // home:  BottomNavBar(),
+       home:  Homescreen(),
 
     );
   }
